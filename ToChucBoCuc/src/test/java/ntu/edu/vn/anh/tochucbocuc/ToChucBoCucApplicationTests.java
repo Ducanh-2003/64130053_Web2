062020d74e3049f5ad6c58a3a9a92f2a3b05a8a8
Nguyễn  Đức Anh <anh.nd.64cntt@ntu.edu.vn>
2025-03-18 13:58:45 +0700

    upload SB_ToChucBoCuc

---
+++ ToChucBoCuc/src/test/java/ntu/edu/vn/anh/tochucbocuc/ToChucBoCucApplicationTests.java
@@ -0,0 +1,13 @@
+package ntu.edu.vn.anh.tochucbocuc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToChucBoCucApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
