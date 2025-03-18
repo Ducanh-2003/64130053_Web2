fff57046e83a60efc42118103293bcb7a0156f83
Nguyễn  Đức Anh <anh.nd.64cntt@ntu.edu.vn>
2025-03-18 12:56:35 +0700

    upload SB_TruyenTuViewSangController

---
+++ SB_TruyenTuViewSangController/src/main/java/ntu/edu/vn/anh/sb_truyentuviewsangcontroller/SbTruyenTuViewSangControllerApplication.java
@@ -0,0 +1,13 @@
+package ntu.edu.vn.anh.sb_truyentuviewsangcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTruyenTuViewSangControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbTruyenTuViewSangControllerApplication.class, args);
+    }
+
+}
