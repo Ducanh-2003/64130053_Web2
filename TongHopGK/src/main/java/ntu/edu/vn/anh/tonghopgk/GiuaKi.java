b570d8779a88e360f66748a3328789be43942f74
Nguyễn  Đức Anh <anh.nd.64cntt@ntu.edu.vn>
2025-03-25 11:04:41 +0700

    3rd commit TongHopGK pj - List, About & Add inside body without bootstrap

--- TongHopGK/src/main/java/ntu/edu/vn/anh/tonghopgk/GiuaKi.java
+++ TongHopGK/src/main/java/ntu/edu/vn/anh/tonghopgk/GiuaKi.java
@@ -2,18 +2,26 @@ package ntu.edu.vn.anh.tonghopgk;
 
 import model.StudentModel;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class GiuaKi {
+
+    private final List<StudentModel> students = new ArrayList<>();
+
+    public GiuaKi() {
+        students.add(new StudentModel("64130053", "Nguyễn Đức Anh", 7));
+        students.add(new StudentModel("64130000", "Nguyễn Thị Thị", 9.9));
+        students.add(new StudentModel("64131234", "Đỗ Văn Đỗ", 4.5));
+        students.add(new StudentModel("64130343", "Lại Lai Lai", 8.8));
+    }
+
     @GetMapping("/")
-    public String giuaKi(@RequestParam(name = "page", defaultValue = "home") String page, ModelMap model) {
+    public String giuaKi(@RequestParam(name = "page", defaultValue = "home") String page, Model model) {
         model.addAttribute("page", page);
 
         if (page.equals("about")) {
@@ -22,18 +30,20 @@ public class GiuaKi {
             model.addAttribute("namSinh", "2003");
             model.addAttribute("gioiTinh", "Nam");
         } else if (page.equals("list")) {
-            List<StudentModel> students = List.of(
-                    new StudentModel("64130053", "Nguyễn Đức Anh", 7),
-                    new StudentModel("64130000", "Nguyễn Thị Thị", 9.9),
-                    new StudentModel("64131234", "Đỗ Văn Đỗ", 4.5),
-                    new StudentModel("64130343", "Lại Lai Lai", 8.8)
-            );
             model.addAttribute("studentModelList", students);
-        }  else if (page.equals("addNew")) {
-
-
-
         }
         return "FrontEndGiuaKi";
+    }
+
+    @GetMapping("/add")
+    public String showAddPage(Model model) {
+        model.addAttribute("student", new StudentModel());
+        return "FrontEndGiuaKi";
+    }
+
+    @PostMapping("/addStudent")
+    public String addStudent(@ModelAttribute StudentModel student) {
+        students.add(student);
+        return "redirect:/?page=list";
     }
 }
