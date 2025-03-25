37849d4f828f8e31ee0dfa29f523e73af9a1717c
Nguyễn  Đức Anh <anh.nd.64cntt@ntu.edu.vn>
2025-03-25 09:12:41 +0700

    init TongHopGK project

---
+++ TongHopGK/src/test/java/ntu/edu/vn/anh/tonghopgk/TongHopGkApplicationTests.java
@@ -0,0 +1,13 @@
+package ntu.edu.vn.anh.tonghopgk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TongHopGkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
