3453b2add416efee0dd15505d077cae95e2731ea
Nguyễn  Đức Anh <anh.nd.64cntt@ntu.edu.vn>
2025-03-25 10:20:01 +0700

    2st commit TongHopGK pj - List & About

--- TongHopGK/src/main/java/ntu/edu/vn/anh/tonghopgk/GiuaKi.java
+++ TongHopGK/src/main/java/ntu/edu/vn/anh/tonghopgk/GiuaKi.java
@@ -1,8 +1,13 @@
 package ntu.edu.vn.anh.tonghopgk;
 
+import model.StudentModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class GiuaKi {
@@ -12,12 +17,23 @@ public class GiuaKi {
     }
 
     @GetMapping("/about")
-    public String about() {
+    public String about(ModelMap model) {
+        model.addAttribute("mssv","64130053");
+        model.addAttribute("hoVaTen","Nguyễn Đức Anh");
+        model.addAttribute("namSinh","64130053");
+        model.addAttribute("gioiTinh","Nam");
         return "layout/About";
     }
 
     @GetMapping("/list")
-    public String list() {
+    public String list(ModelMap model) {
+        List<StudentModel> students = List.of(
+                new StudentModel("64130053", "Nguyễn Đức Anh", 7),
+                new StudentModel("64130000", "Nguyễn Thị Thị", 9.9),
+                new StudentModel("64131234", "Đỗ Văn Đỗ", 4.5),
+                new StudentModel("64130343", "Lại Lai Lai", 8.8)
+        );
+        model.addAttribute("studentModelList", students);
         return "layout/List";
     }
 
