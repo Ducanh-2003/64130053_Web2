550811ba5e7c10a6c534aeebf2a342afabcb1b84
Nguyễn  Đức Anh <anh.nd.64cntt@ntu.edu.vn>
2025-03-25 09:47:12 +0700

    1st commit TongHopGK pj

--- TongHopGK/src/main/java/ntu/edu/vn/anh/tonghopgk/GiuaKi.java
+++ TongHopGK/src/main/java/ntu/edu/vn/anh/tonghopgk/GiuaKi.java
@@ -2,11 +2,27 @@ package ntu.edu.vn.anh.tonghopgk;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GiuaKi {
     @GetMapping("/")
     public String giuaKi() {
         return "FrontEndGiuaKi";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "layout/About";
+    }
+
+    @GetMapping("/list")
+    public String list() {
+        return "layout/List";
+    }
+
+    @RequestMapping("/addNew")
+    public String addNew() {
+        return "layout/AddNew";
     }
 }
