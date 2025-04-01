3c46679df23b70abb1c6fdd9842217696fe90afd
Nguyễn  Đức Anh <anh.nd.64cntt@ntu.edu.vn>
2025-04-01 09:00:43 +0700

    init nguyenducanh_ThiGiuaKy

---
+++ nguyenducanh_ThiGiuaKy/src/main/java/thiGK/ntu64130053/NguyenducanhThiGiuaKyApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64130053;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenducanhThiGiuaKyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NguyenducanhThiGiuaKyApplication.class, args);
+    }
+
+}
---
+++ nguyenducanh_ThiGiuaKy/src/test/java/thiGK/ntu64130053/NguyenducanhThiGiuaKyApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64130053;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenducanhThiGiuaKyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
