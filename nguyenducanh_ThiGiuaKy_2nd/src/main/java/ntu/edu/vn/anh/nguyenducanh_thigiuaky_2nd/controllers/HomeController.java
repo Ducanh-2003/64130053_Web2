85249185a293de1568b884976b0b589196a3d895
Nguyễn  Đức Anh <anh.nd.64cntt@ntu.edu.vn>
2025-04-15 22:35:22 +0700

    1st commit Layout & Dashboard

---
+++ nguyenducanh_ThiGiuaKy_2nd/src/main/java/ntu/edu/vn/anh/nguyenducanh_thigiuaky_2nd/controllers/HomeController.java
@@ -0,0 +1,22 @@
+package ntu.edu.vn.anh.nguyenducanh_thigiuaky_2nd.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String dashBoard(ModelMap model) {
+        model.addAttribute("mssv", "64130053");
+        model.addAttribute("hoVaTen", "Nguyễn Đức Anh");
+        model.addAttribute("namSinh", "64130053");
+        model.addAttribute("gioiTinh", "Male");
+        return "Dashboard";
+    }
+    @RequestMapping("/layout")
+    public String layout(ModelMap model) {
+        model.addAttribute("hoVaTen", "Nguyễn Đức Anh");
+        return "FrontEndLayout";
+    }
+}
---
+++ nguyenducanh_ThiGiuaKy_2nd/src/test/java/ntu/edu/vn/anh/nguyenducanh_thigiuaky_2nd/NguyenducanhThiGiuaKy2ndApplicationTests.java
@@ -0,0 +1,13 @@
+package ntu.edu.vn.anh.nguyenducanh_thigiuaky_2nd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenducanhThiGiuaKy2ndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
